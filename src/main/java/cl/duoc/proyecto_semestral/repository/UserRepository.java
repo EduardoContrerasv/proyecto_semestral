75a2a410711ba416ca7b36220fad94d2d19a5c84
Dario Sagredo <dariosagredo@gmail.com>
2026-05-06 23:46:08 -0400

    Repository creado

---
+++ src/main/java/cl/duoc/proyecto_semestral/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package cl.duoc.proyecto_semestral.repository;
+
+import cl.duoc.proyecto_semestral.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+
+    private List<User> usersList new ArrayList<>;
+}
