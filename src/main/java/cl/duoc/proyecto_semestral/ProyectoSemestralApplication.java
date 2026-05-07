14d328b024b546bc6f28df446474860f46835a1b
EduardoCV <edu.contrerasv@duocuc.cl>
2026-05-06 23:34:53 -0400

    test

---
+++ src/main/java/cl/duoc/proyecto_semestral/ProyectoSemestralApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.proyecto_semestral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSemestralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSemestralApplication.class, args);
+	}
+
+}
